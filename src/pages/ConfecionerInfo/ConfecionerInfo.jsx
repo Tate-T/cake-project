a896af98b49f8cef17b6ce304541ce168237f489
Gorbunov Kirill <1kvgxiaomi1711@gmail.com>
2024-12-28 02:34:43 +0200

    Add confectioner info, deserts, cabinet

---
+++ src/pages/ConfecionerInfo/ConfecionerInfo.jsx
@@ -0,0 +1,128 @@
+import React from 'react';
+// import Header from "../../components/Header/Header";
+import Searchbar from "../../components/Searchbar/Searchbar";
+import Footer from "../../components/Footer/Footer";
+import aboutConfectionerImg from "../../imgs/aboutConfectionerImg.jpg";
+import { Link } from 'react-router-dom';
+import cakeImg1 from '../../imgs/cakes/cake-1.jpg';
+import cakeImg2 from '../../imgs/cakes/cake-2.jpg';
+import cakeImg3 from '../../imgs/cakes/cake-3.jpg';
+import cakeImg4 from '../../imgs/cakes/cake-4.jpg';
+import cakeImg5 from '../../imgs/cakes/cake-5.jpg';
+import css from './ConfecionerInfo.module.css';
+
+const safeArr = [];
+function createUniqueId() {
+    const id = "id" + Math.random().toString(16).slice(2);
+    if (!safeArr.includes(id)) {
+        safeArr.push(id);
+        return id;
+    }
+    return createUniqueId();
+}
+
+export default () => {
+    return (
+        <>
+            {/* <Header /> */}
+            <div className={css.container}>
+                <div className={css.searchbar}>
+                    <Searchbar placeholder="Шоколадний торт з квітами" />
+                </div>
+                <div className={css.main}>
+                    <ul className={css.confectionerList}>
+                        <li>
+                            <Link to="/confectioner/about">
+                                <button className={`${css.menuBtn} ${css['menuBtn--confectionerActive']}`}>
+                                    Про кондитера
+                                </button>
+                            </Link>
+                        </li>
+                        <li>
+                            <Link to="/confectioner/products">
+                                <button className={css.menuBtn}>Товари</button>
+                            </Link>
+                        </li>
+                    </ul>
+                    <div style={{ display: "block" }}>
+                        <div className={css.info}>
+                            <img src={aboutConfectionerImg} alt="confectioner" className={css.userPic} />
+                            <div>
+                                <div className={css.city}>Київ</div>
+                                <div className={css.userName}>Інна Пономаренко</div>
+                                <button className={css.contactBtn}>Переглянути контакти</button>
+                            </div>
+                        </div>
+                        <div className={css.infoText}>
+                            Вже понад 10 років удосконалюю свої навички. <br></br>
+                            З 2011 по 2017 – засновник бренду Марципан у Вінниці зі штатом 35 осіб. Я була шеф-кондитером 3 ресторанів. А також, одним із перших кондитерів, який ввів у моду кенді бари в Чорнозем'ї та спільно з партнерами придумали коробки квіти та макаронів – ідея яка розлетілася по всьому світу.<br></br>
+                            Також, я та моя команда займаємося організацією та запуском кондитерських виробництв.<br></br>
+                            Маю великий досвід за плечима. За 10 років роботи я створила тисячі та тисячі тортів.<br></br>
+                            Пройшовши великий шлях від домашнього кондитера до бренд-шефа, я з великою радістю та впевненістю ділюся з учнями своїми знаннями від випічки тортів та тістечок до масштабних весільних тортів.
+                        </div>
+                        <div className={css.reviews}>
+                            <div className={css.reviewsTitle}>Відгуки</div>
+                            <ul className={css.reviewsList}>
+                                {[{
+                                    name: 'Анна Щедріна', date: '15.11.2020', title: 'Торт “Червоний оксамит”', content: 'Замовляли на мій День народження мусовий торт із маракуєю та залишилися дуже задоволені. Тортик вийшов дуже смачний, з приємною кислинкою за рахунок маракуї та неймовірно ніжною та легкою текстурою. Начинка, на мій смак, просто шикарна: желе маракуйї, бісквіт, хрусткий шар та мус із манго та маракуйї.' 
+                                }, {
+                                    name: 'Олександр Ольцев', date: '15.11.2020', title: 'Торт весільний з фруктами', content: 'Супер десерти для дому та хореки. Чарівні круасани. Торти, кейки та мусові десерти - це кохання'
+                                }, {
+                                    name: 'Олександр Ольцев', date: '15.11.2020', title: 'Торт для дня народження з квітами, 5 кг', content: "Найсмачніші торти та тістечка, казкові еклери. У захваті від десертів. Кондитера - чарівниці, доставка завжди на найвищому рівні!!!!! Всій сім'єю обожнюємо Вашу кондитерську , дякую за вашу працю і смакоти!! <3"
+                                }].map((review, index) => (
+                                    <li key={index} className={css.reviewItem}>
+                                        <div className={css.reviewer}>
+                                            <span className={css.reviewerName}>{review.name}</span>
+                                            <span className={css.reviewDate}>{review.date}</span>
+                                        </div>
+                                        <div className={css.reviewTitle}>{review.title}</div>
+                                        <div className={css.reviewContent}>{review.content}</div>
+                                  </li>
+                                ))}
+                            </ul>
+                        </div>
+                        <button className={css.loadMore}>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="25" height="26" viewBox="0 0 25 26" fill="none">
+                                <g clip-path="url(#clip0_5900_11473)">
+                                    <path d="M14.5833 13C14.5833 11.8542 13.6458 10.9167 12.5 10.9167C11.3542 10.9167 10.4167 11.8542 10.4167 13C10.4167 14.1458 11.3542 15.0833 12.5 15.0833C13.6458 15.0833 14.5833 14.1458 14.5833 13ZM12.5 3.625C7.32292 3.625 3.125 7.82292 3.125 13H0L4.16667 17.1667L8.33333 13H5.20833C5.20833 8.96875 8.46875 5.70833 12.5 5.70833C16.5312 5.70833 19.7917 8.96875 19.7917 13C19.7917 17.0312 16.5312 20.2917 12.5 20.2917C10.9271 20.2917 9.46875 19.7812 8.27083 18.9375L6.79167 20.4375C8.375 21.6458 10.3542 22.375 12.5 22.375C17.6771 22.375 21.875 18.1771 21.875 13C21.875 7.82292 17.6771 3.625 12.5 3.625Z" fill="#43607C"/>
+                                </g>
+                                <defs>
+                                    <clipPath id="clip0_5900_11473">
+                                        <rect width="25" height="25" fill="white" transform="translate(0 0.5)"/>
+                                    </clipPath>
+                                </defs>
+                            </svg>
+                            Більше відгуків
+                        </button>
+                        <ul className={css.pagination}>
+                            <li className={css.arrow}>&#x3c;</li>
+                            {[1, 2, 3, 4, 5].map(page => (
+                                <li key={createUniqueId()}>{page}</li>
+                            ))}
+                            <li className={css.arrow}>&#x3e;</li>
+                        </ul>
+                    </div>
+                </div>
+                <div className={css.goods}>
+                    <div className={css.good}>
+                        <div className={css.goodTitle}>Торти</div>
+                        <div className={css.goodActions}>
+                            <button className={css.goodBtn}>Переглянути всі</button>
+                        </div>
+                    </div>
+                    <div className={css.goodList}>
+                        {[cakeImg1, cakeImg2, cakeImg3, cakeImg4, cakeImg5].map((img, index) => (
+                            <div key={index} className={css.goodItem}>
+                                <img src={img} alt="cake" className={css.goodItemImg} />
+                                <div className={css.goodItemPrice}>520 грн</div>
+                                <div className={css.goodItemContent}>Опис торта</div>
+                                <div className={css.goodItemOptions}>4 варіанти начинок</div>
+                            </div>
+                        ))}
+                    </div>
+                </div>
+            </div>
+      <Footer />
+    </>
+  );
+};
---
+++ src/pages/ConfectionerCabinet/ConfectionerCabinet.jsx
@@ -0,0 +1,214 @@
+import { Link, NavLink } from 'react-router-dom';
+import Footer from "../../components/Footer/Footer";
+// import Header from "../../components/Header/Header";
+import React from "react";
+import css from "./ConfectionerCabinet.module.css";
+
+export default ({}) => {
+    return (
+        <>
+            {/* <Header /> */}
+            <section>
+            <div className={css.container}>
+                <div className={css.navCont}>
+                    <div className={css.navigation}>
+                        <h2 className={css.navTitle}>Загальне</h2>
+                        <ul className={css.confectionerList}>
+                            <li>
+                                <button className={`${css.confectionerList__button} ${css['confectionerList__button--active']}`}>
+                                    Інформація про кондитера
+                                </button>
+                            </li>
+                            <li>
+                                <button className={css.confectionerList__button}>
+                                    Заявки
+                                </button>
+                            </li>
+                            <li>
+                                <button className={css.confectionerList__button}>
+                                    Відгуки
+                                </button>
+                            </li>
+                        </ul>
+                        <h2 className={css.navTitle}>Категорії</h2>
+                        <ul className={css.categories}>
+                            <li>
+                                <button className={css.categories__button}>Всі товари</button>
+                            </li>
+                            <li>
+                                <button className={css.categories__button}>Торти</button>
+                            </li>
+                            <li>
+                                <button className={css.categories__button}>Макаруни</button>
+                            </li>
+                            <li>
+                                <button className={css.categories__button}>Капкейки</button>
+                            </li>
+                            <li>
+                                <button className={`${css.categories__button} ${css['categories__button--add']}`}>
+                                    <div className={css.addIcon}>
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" fill="none">
+                                            <path d="M10 17.5C14.1421 17.5 17.5 14.1421 17.5 10C17.5 5.85786 14.1421 2.5 10 2.5C5.85786 2.5 2.5 5.85786 2.5 10C2.5 14.1421 5.85786 17.5 10 17.5Z" stroke="#8E4A4E" stroke-width="2" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"/>
+                                            <path d="M6.6665 10H13.3332" stroke="#8E4A4E" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                                            <path d="M10 13.3334L10 6.66675" stroke="#8E4A4E" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                                        </svg>
+                                    </div>
+                                    Додати категорію
+                                </button>
+                            </li>
+                        </ul>
+                    </div>
+                    <div className={css.mainComps}>
+                        <h2 className={css.titleSec}>Фото</h2>
+                        <p className={css.description}>Додайте сюди своє фото або логотип компанії. Кондитерів з фото обирають частіше</p>
+                        <div className={css.photoCont}>
+                            <img className={css.photoCont__photo} src="https://i.ibb.co/s5LbyW4/image.png" alt="Фото кондитера" />
+                            <div className={css.butsCont}>
+                                <button className={css.photoBut}>
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" fill="none">
+                                        <g clip-path="url(#clip0_5881_10387)">
+                                            <path d="M15.8333 5.83341V8.32508C15.8333 8.32508 14.175 8.33341 14.1667 8.32508V5.83341H11.6667C11.6667 5.83341 11.675 4.17508 11.6667 4.16675H14.1667V1.66675H15.8333V4.16675H18.3333V5.83341H15.8333ZM13.3333 9.16675V6.66675H10.8333V4.16675H4.16667C3.25 4.16675 2.5 4.91675 2.5 5.83341V15.8334C2.5 16.7501 3.25 17.5001 4.16667 17.5001H14.1667C15.0833 17.5001 15.8333 16.7501 15.8333 15.8334V9.16675H13.3333ZM4.16667 15.8334L6.66667 12.5001L8.33333 15.0001L10.8333 11.6667L14.1667 15.8334H4.16667Z" fill="#84A6C2"/>
+                                        </g>
+                                        <defs>
+                                            <clipPath id="clip0_5881_10387">
+                                                <rect width="20" height="20" fill="white"/>
+                                            </clipPath>
+                                        </defs>
+                                    </svg>
+                                    Замінити фото
+                                </button>
+                                <button className={css.photoBut}>
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="21" height="20" viewBox="0 0 21 20" fill="none">
+                                        <g clip-path="url(#clip0_5881_10113)">
+                                            <path d="M5.49984 15.8333C5.49984 16.75 6.24984 17.5 7.1665 17.5H13.8332C14.7498 17.5 15.4998 16.75 15.4998 15.8333V5.83333H5.49984V15.8333ZM16.3332 3.33333H13.4165L12.5832 2.5H8.4165L7.58317 3.33333H4.6665V5H16.3332V3.33333Z" fill="#84A6C2"/>
+                                        </g>
+                                      <defs>
+                                        <clipPath id="clip0_5881_10113">
+                                          <rect width="20" height="20" fill="white" transform="translate(0.5)"/>
+                                        </clipPath>
+                                      </defs>
+                                    </svg>
+                                    Видалити
+                                </button>
+                            </div>
+                        </div>
+                        <h2 className={css.titleSec}>Загальне
+                            <div className={css.editIcon}>
+                                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" fill="none">
+                                    <g clip-path="url(#clip0_5881_5007)">
+                                        <path d="M2.5 14.3749V17.4999H5.625L14.8417 8.28322L11.7167 5.15822L2.5 14.3749ZM17.2583 5.86655C17.5833 5.54155 17.5833 5.01655 17.2583 4.69155L15.3083 2.74155C14.9833 2.41655 14.4583 2.41655 14.1333 2.74155L12.6083 4.26655L15.7333 7.39155L17.2583 5.86655Z" fill="#A63A50"/>
+                                    </g>
+                                    <defs>
+                                        <clipPath id="clip0_5881_5007">
+                                            <rect width="20" height="20" fill="white"/>
+                                        </clipPath>
+                                    </defs>
+                                </svg>
+                            </div>
+                        </h2>
+                        <p className={css.label}>Ім'я кондитера або назва кондитерської</p>
+                        <p className={css.dataArea}>Інна Пономаренко</p>
+                        <p className={css.label}>Опис</p>
+                        <p className={`${css.dataArea} ${css['dataArea--last']}`}></p>
+                        <h2 className={css.titleSec}>Контакти
+                            <div className={css.editIcon}>
+                                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" fill="none">
+                                    <g clip-path="url(#clip0_5881_5007)">
+                                        <path d="M2.5 14.3749V17.4999H5.625L14.8417 8.28322L11.7167 5.15822L2.5 14.3749ZM17.2583 5.86655C17.5833 5.54155 17.5833 5.01655 17.2583 4.69155L15.3083 2.74155C14.9833 2.41655 14.4583 2.41655 14.1333 2.74155L12.6083 4.26655L15.7333 7.39155L17.2583 5.86655Z" fill="#A63A50"/>
+                                    </g>
+                                    <defs>
+                                        <clipPath id="clip0_5881_5007">
+                                            <rect width="20" height="20" fill="white"/>
+                                        </clipPath>
+                                    </defs>
+                                </svg>
+                            </div>
+                        </h2>
+                        <p className={css.label}>Адреса</p>
+                        <p className={css.dataArea}>Київ, Правди 15</p>
+                        <p className={css.label}>Номер телефону</p>
+                        <p className={css.dataArea}>+56 085 345 76 34</p>
+                        <p className={css.label}>Пошта</p>
+                        <p className={`${css.dataArea} ${css['dataArea--last']}`}>Mail@example.com</p>
+                        <h2 className={css.titleSec}>Доставка
+                            <div className={css.editIcon}>
+                                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" fill="none">
+                                    <g clip-path="url(#clip0_5881_5007)">
+                                        <path d="M2.5 14.3749V17.4999H5.625L14.8417 8.28322L11.7167 5.15822L2.5 14.3749ZM17.2583 5.86655C17.5833 5.54155 17.5833 5.01655 17.2583 4.69155L15.3083 2.74155C14.9833 2.41655 14.4583 2.41655 14.1333 2.74155L12.6083 4.26655L15.7333 7.39155L17.2583 5.86655Z" fill="#A63A50"/>
+                                    </g>
+                                    <defs>
+                                        <clipPath id="clip0_5881_5007">
+                                            <rect width="20" height="20" fill="white"/>
+                                        </clipPath>
+                                    </defs>
+                                </svg>
+                            </div>
+                        </h2>
+                        <p className={`${css.dataArea} ${css['dataArea--moreInfo']}`}>
+                            <ul>
+                                <li>Cамовивіз, Київ, Симиренка 23а</li>
+                                <li>Власна доставка, 150 грн</li>
+                            </ul>
+                        </p>
+                        <h2 className={css.titleSec}>Оплата
+                            <div className={css.editIcon}>
+                                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" fill="none">
+                                    <g clip-path="url(#clip0_5881_5007)">
+                                        <path d="M2.5 14.3749V17.4999H5.625L14.8417 8.28322L11.7167 5.15822L2.5 14.3749ZM17.2583 5.86655C17.5833 5.54155 17.5833 5.01655 17.2583 4.69155L15.3083 2.74155C14.9833 2.41655 14.4583 2.41655 14.1333 2.74155L12.6083 4.26655L15.7333 7.39155L17.2583 5.86655Z" fill="#A63A50"/>
+                                    </g>
+                                    <defs>
+                                        <clipPath id="clip0_5881_5007">
+                                            <rect width="20" height="20" fill="white"/>
+                                        </clipPath>
+                                    </defs>
+                                </svg>
+                            </div>
+                        </h2>
+                        <p className={`${css.dataArea} ${css['dataArea--moreInfo']}`}>
+                            <ul>
+                                <li>Передплата 50%</li>
+                            </ul>
+                        </p>
+                        <h2 className={css.titleSec}>Соціальні мережі
+                            <div className={css.editIcon}>
+                                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" fill="none">
+                                    <g clip-path="url(#clip0_5881_5007)">
+                                        <path d="M2.5 14.3749V17.4999H5.625L14.8417 8.28322L11.7167 5.15822L2.5 14.3749ZM17.2583 5.86655C17.5833 5.54155 17.5833 5.01655 17.2583 4.69155L15.3083 2.74155C14.9833 2.41655 14.4583 2.41655 14.1333 2.74155L12.6083 4.26655L15.7333 7.39155L17.2583 5.86655Z" fill="#A63A50"/>
+                                    </g>
+                                    <defs>
+                                        <clipPath id="clip0_5881_5007">
+                                            <rect width="20" height="20" fill="white"/>
+                                        </clipPath>
+                                    </defs>
+                                </svg>
+                            </div>
+                        </h2>
+                        <p className={css.label}>Instagram</p>
+                        <p className={css.dataArea}>www.instagram.com</p>
+                        <p className={css.label}>Facebook</p>
+                        <p className={`${css.dataArea} ${css['dataArea--last']}`}>www.facebook.com</p>
+                        <div className={css.profFillCont}>
+                            <h2 className={css.profFillCont__title}>Ваш профіль заповнений на 83%</h2>
+                            <div className={css.barCont}>
+                                <div className={css.fieldBar}>
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="698" height="10" viewBox="0 0 698 10" fill="none">
+                                        <path d="M5.5 1H692.5C694.709 1 696.5 2.79086 696.5 5C696.5 7.20914 694.709 9 692.5 9H5.5C3.29086 9 1.5 7.20914 1.5 5C1.5 2.79086 3.29086 1 5.5 1Z" stroke="#84A6C2" stroke-width="2" stroke-linecap="round"/>
+                                    </svg>
+                                </div>
+                                <div className={css.fillBar}>
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="501" height="10" viewBox="0 0 501 10" fill="none">
+                                        <path d="M4.99999 0H496C498.761 0 501 2.23858 501 5C501 7.76142 498.761 10 496 10H5C2.23858 10 0 7.76142 0 5C0 2.23858 2.23857 0 4.99999 0Z" fill="#011936"/>
+                                    </svg> 
+                                </div>
+                            </div>
+                            <p className={css.descrFill}>Розкажіть про себе все, що може бути цікаво користувачам. Заповнені профілі користуються більшою популярністю</p>
+                            <button className={css.fillBut}>Заповнити поле “Опис”</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            </section>
+            <Footer />
+        </>
+    )
+}
