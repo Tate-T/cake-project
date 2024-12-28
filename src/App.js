92dbb5a2d2a90275e2a2aa42c8639479bf40c5e7
StepanRychikhin2 <111581882+StepanRychikhin2@users.noreply.github.com>
2024-12-28 12:13:10 +0200

    save

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
 import { Routes, Route } from "react-router-dom";
-
+import Footer from "./components/Footer/Footer";
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Main />} />
-      <Route path="/desserts" element={<Desserts />} />
-      <Route path="/confecionres" element={<Confecioners />} />
-      <Route path="/articles" element={<Articles />} />
-      <Route path="/articles/:id" element={<ArticlePage />} />
-      <Route path="/info-confecoiners" element={<InfoConfecoiners />} />
-      <Route path="/confectioner">
-        <Route path="products" element={<ConfectionerProducts />} />
-        <Route path="about" element={<AboutConfectioner />} />
-      </Route>
-    </Routes>
+    <>
+    <Footer></Footer>
+    </>
+    // <Routes>
+    //   <Route path="/" element={<Main />} />
+    //   <Route path="/desserts" element={<Desserts />} />
+    //   <Route path="/confecionres" element={<Confecioners />} />
+    //   <Route path="/articles" element={<Articles />} />
+    //   <Route path="/articles/:id" element={<ArticlePage />} />
+    //   <Route path="/info-confecoiners" element={<InfoConfecoiners />} />
+    //   <Route path="/confectioner">
+    //     <Route path="products" element={<ConfectionerProducts />} />
+    //     <Route path="about" element={<AboutConfectioner />} />
+    //   </Route>
+    // </Routes>
   );
 }
 
