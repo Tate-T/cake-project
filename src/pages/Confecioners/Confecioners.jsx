2ce28e6c92da874d191a5ea15fe604f9637e8e9c
TimurCherin <109847059+TimurCherin@users.noreply.github.com>
2025-01-06 18:46:33 +0200

    confecioners

--- src/pages/Confecioners/Confecioners.jsx
+++ src/pages/Confecioners/Confecioners.jsx
@@ -1,65 +1,51 @@
-import React from "react";
-import Searchbar from "../../components/Searchbar/Searchbar";
-import Footer from "../../components/Footer/Footer";
-import aboutConfectionerImg from "../../imgs/aboutConfectionerImg.jpg";
-import { Link } from "react-router-dom";
-import cakeImg1 from "../../imgs/cakes/cake-1.jpg";
-import cakeImg2 from "../../imgs/cakes/cake-2.jpg";
-import cakeImg3 from "../../imgs/cakes/cake-3.jpg";
-import cakeImg4 from "../../imgs/cakes/cake-4.jpg";
-import css from "./Confecioners.module.css";
+import React from 'react';
+import HeaderComp from '../../components/general-components/Header/Header';
+import { Container } from '../../components/general-components/Container/Container.styled';
+import { ConfectionersList } from '../../components/general-components/ConfectionersList/ConfectionersList';
+import Footerr from '../../components/general-components/Footer/Footer';
+import ListState from '../../components/general-components/upFooterListState/ListState';
+import {
+  HeroSearchInputBox,
+  HeroSearchInput,
+  HeroSearchBtn,
+  HeroPopularSearchList,
+  HeroPopularSearchItem,
+  HeroPopularSearchText,
+  BtnLoading,
+  LoadingList,
+  LoadingBtn,
+} from './Confecioners.styled';
 
-const Confectioners = () => {
+export const Confecioners = () => {
   return (
     <>
-      <div className={css.container}>
-        <div className={css.searchbarWrapper}>
-          <Searchbar placeholder="Шоколадний торт з квітами" />
-        </div>
-        <div className={css.confectionersSection}>
-          {[...Array(4)].map((_, index) => (
-            <div key={index} className={css.confectionerCard}>
-              <Link to="/confectioner/about" className={css.confectionerHeader}>
-                <img
-                  src={aboutConfectionerImg}
-                  alt="confectioner"
-                  className={css.confectionerImage}
-                />
-                <div className={css.confectionerDetails}>
-                  <div className={css.confectionerName}>Інна Пономаренко</div>
-                  <div className={css.confectionerExperience}>23 відгуки</div>
-                  <div className={css.confectionerTags}>
-                    <span className={css.tag}>Торти</span>
-                    <span className={css.tag}>Цукерки</span>
-                    <span className={css.tag}>+3</span>
-                  </div>
-                </div>
-              </Link>
-              <div className={css.confectionerGoods}>
-                {[cakeImg1, cakeImg2, cakeImg3, cakeImg4].map((cake, idx) => (
-                  <div key={idx} className={css.goodItem}>
-                    <img src={cake} alt="cake" className={css.goodImage} />
-                    <div className={css.goodPrice}>520 грн</div>
-                    <div className={css.goodDescription}>Опис торта</div>
-                    <div className={css.goodVariants}>4 варіанти начинок</div>
-                  </div>
-                ))}
-              </div>
-              <button className={css.viewAllButton}>Переглянути всі</button>
-            </div>
+      <HeaderComp />
+      <Container>
+        <HeroSearchInputBox>
+          <HeroSearchInput />
+          <HeroSearchBtn type="button">Шукати</HeroSearchBtn>
+        </HeroSearchInputBox>
+        <HeroPopularSearchList>
+          {['Капкейки', 'Торти', 'Кейк-попси', 'Макаруни', 'Печиво', 'Цукерки'].map((item) => (
+            <HeroPopularSearchItem key={item}>
+              <HeroPopularSearchText>{item}</HeroPopularSearchText>
+            </HeroPopularSearchItem>
           ))}
-        </div>
-        <div className={css.paginationSection}>
-          <button className={css.paginationButton}>1</button>
-          <button className={css.paginationButton}>2</button>
-          <button className={css.paginationButton}>3</button>
-          <span className={css.paginationEllipsis}>...</span>
-          <button className={css.paginationButton}>15</button>
-        </div>
-        <Footer />
-      </div>
+        </HeroPopularSearchList>
+        <ConfectionersList />
+        <BtnLoading type="button">
+          Більше смаколиків
+        </BtnLoading>
+        <LoadingList>
+          {[1, 2, 3, 4, '...', 15].map((item, index) => (
+            <li key={index}>
+              <LoadingBtn type="button">{item}</LoadingBtn>
+            </li>
+          ))}
+        </LoadingList>
+        <ListState />
+      </Container>
+      <Footerr />
     </>
   );
 };
-
-export default Confectioners;
---
+++ src/pages/Confecioners/Confecioners.styled.jsx
@@ -0,0 +1,94 @@
+import styled from 'styled-components';
+import { List } from '../../components/general-components/List/List.styled';
+
+export const HeroSearchInputBox = styled.div`
+  display: flex;
+  margin-top: 30px;
+  margin-bottom: 40px;
+
+  button:hover {
+    cursor: pointer;
+  }
+`;
+
+export const HeroSearchInput = styled.input`
+  border-radius: 5px 0 0 5px;
+  border: 1px solid #dfdfdf;
+  width: 1340px;
+  height: 48px;
+  padding: 0 20px;
+`;
+
+export const HeroSearchBtn = styled.button`
+  padding: 14px 30px;
+  border-radius: 0 5px 5px 0;
+  border: 1px solid #fdad6d;
+  background-color: ${(props) => props.theme.colors.buttonsColor};
+  color: #8e4a4e;
+  font-family: 'Verdana';
+  font-size: ${(props) => props.theme.spacings(4.5)};
+  height: 50px;
+`;
+
+export const HeroPopularSearchList = styled(List)`
+  max-width: 800px;
+  gap: 10px;
+  margin: 30px auto 40px;
+  justify-content: left;
+`;
+
+export const HeroPopularSearchItem = styled.li`
+  border-radius: 40px;
+  background-color: #fff2f2;
+  padding: 5px 20px;
+  height: 36px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  &:hover {
+    cursor: pointer;
+  }
+`;
+
+export const HeroPopularSearchText = styled.p`
+  color: #8e4a4e;
+  font-family: 'Verdana';
+  font-size: 16px;
+`;
+
+export const BtnLoading = styled.button`
+  margin-top: 30px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: transparent;
+  color: var(--CTA-blue, #43607C);
+  font-family: Inter;
+  font-size: 16px;
+  border-radius: 49px;
+  border: 1px solid var(--CTA-blue-text, #84A6C2);
+  padding: 5px 10px;
+  width: 100%;
+`;
+
+export const LoadingList = styled.ul`
+  justify-content: center;
+  align-items: center;
+  width: 435px;
+  gap: 10px;
+  margin: 40px auto 80px;
+  display: flex;
+  flex-wrap: wrap;
+`;
+
+export const LoadingBtn = styled.button`
+  border-radius: 50px;
+  background-color: transparent;
+  width: 50px;
+  height: 50px;
+  font-family: Inter;
+  font-size: 18px;
+  border: 1px solid var(--CTA-yellow-border, #FFE2CA);
+  background: var(--White, #FFF);
+`;
