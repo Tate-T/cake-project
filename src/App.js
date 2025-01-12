996eb6b0bf1c11ce04d11a40ec8d1a5019d83f67
diana7770 <104677361+diana7770@users.noreply.github.com>
2025-01-12 15:51:10 +0200

    desserts

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import { Routes, Route, Navigate } from "react-router-dom";
 import { lazy, Suspense } from "react";
-// import DessertInfo from "./pages/DessertInfo/DessertInfo";
-// const DessertInfo = lazy(() => import("./pages/DessertInfo/DessertInfo"));
 
 // import Articles from "./pages/Articles/Articles";
 const Articles = lazy(() => import("./pages/Articles/Articles"));
@@ -9,18 +7,27 @@ const Articles = lazy(() => import("./pages/Articles/Articles"));
 // import Article from "./pages/Article/Article";
 const Article = lazy(() => import("./pages/Article/Article"));
 
+// import Desserts from "./pages/Desserts/Desserts";
+const Desserts = lazy(() => import("./pages/Desserts/Desserts"));
+
 // import Main from "./pages/Main/Main";
 const Main = lazy(() => import("./pages/Main/Main"));
 // import Confecioners from './pages/Confecioners/Confecioners';
 
 // import ConfectionerCabinet from "./pages/ConfectionerCabinet/ConfectionerCabinet";
-const ConfectionerCabinet = lazy(() => import("./pages/ConfectionerCabinet/ConfectionerCabinet"));
+const ConfectionerCabinet = lazy(() =>
+  import("./pages/ConfectionerCabinet/ConfectionerCabinet")
+);
 
 // import ConfecionerInfo from "./pages/ConfecionerInfo/ConfecionerInfo";
-const ConfecionerInfo = lazy(() => import("./pages/ConfecionerInfo/ConfecionerInfo"));
+const ConfecionerInfo = lazy(() =>
+  import("./pages/ConfecionerInfo/ConfecionerInfo")
+);
 
 // import ConfectionerDeserts from "./pages/ConfectionerDeserts/ConfectionerDeserts";
-const ConfectionerDeserts = lazy(() => import("./pages/ConfectionerDeserts/ConfectionerDeserts"));
+const ConfectionerDeserts = lazy(() =>
+  import("./pages/ConfectionerDeserts/ConfectionerDeserts")
+);
 
 // import Birthday from "./pages/Main/SubPages/Birthday/Birthday";
 const Birthday = lazy(() => import("./pages/Main/SubPages/Birthday/Birthday"));
@@ -32,7 +39,9 @@ const Cupcakes = lazy(() => import("./pages/Main/SubPages/Cupcakes/Cupcakes"));
 const Patriot = lazy(() => import("./pages/Main/SubPages/Patriot/Patriot"));
 
 // import ConfectionerLogin from "./components/ConfectionerLogin/ConfectionerLogin";
-const ConfectionerLogin = lazy(() => import("./components/ConfectionerLogin/ConfectionerLogin"));
+const ConfectionerLogin = lazy(() =>
+  import("./components/ConfectionerLogin/ConfectionerLogin")
+);
 
 // hello
 
@@ -54,7 +63,7 @@ function App() {
           <Route path="flowers" element={<></>} />
           <Route path="classic" element={<></>} />
         </Route>
-        {/* <Route path="/desserts" element={<Desserts />} /> */}
+        <Route path="/desserts" element={<Desserts />} />
         {/* <Route path="/desserts/:id" element={<DessertInfo />} /> */}
 
         {/* <Route path="/confecioners" element={<Confecioners />} /> */}
