74544f22ea6c3899b44122c82abbb636f40eb2a1
StepanRychikhin2 <111581882+StepanRychikhin2@users.noreply.github.com>
2025-03-15 12:13:28 +0200

    saveFormatt

--- src/components/BestConfectioners/BestConfectioners.jsx
+++ src/components/BestConfectioners/BestConfectioners.jsx
@@ -11,7 +11,7 @@ import topImg3 from '../../imgs/confectioners/homeBakery.png'
 import { getListCondist } from '../../redux/bestConditersMain/topCondistWere'
 import { retry } from '@reduxjs/toolkit/query'
 import { nanoid } from 'nanoid'
-import { listCondSelector } from './selectors'
+import { listCondSelector } from '../../redux/bestConditersMain/selectors'
 const animals = [topImg1, topImg2, topImg3]
 export default function ListTopPip() {
 	const dispatch = useDispatch()
